92241ce67853575a2f88a856efa79495ce3c83aa
vtsao90 <simply.vivacious@gmail.com>
2018-09-11 08:37:29 -0700

    Add files via upload

--- itin_gen/api_itin.py
+++ itin_gen/api_itin.py
@@ -5,26 +5,26 @@ def itin_generator(user_preferences, budget=2000, ambition = [9, 17], temperatur
     import matplotlib.pyplot as plt
     import math
 
-    from data_gen import get_fake_data
-    from initial_route import create_initial_route
-    from test_locations import get_start_and_stop
-    from Path import Path
-    from Fitness import Fitness
+    # from data_gen import get_fake_data
+    # from initial_route import create_initial_route
+    # from test_locations import get_start_and_stop
+    # from Path import Path
+    # from Fitness import Fitness
     import test2
-    from test2 import propagate_change
-    np.random.seed(52)
+
+    np.random.seed(55)
 
     # for current testing purposes
     tour_length = 60*(ambition[1] - ambition[0])
 
-    site_name_lookup, SF_sites, travel_matrix = get_fake_data(user_preferences)
+    site_name_lookup, SF_sites, travel_matrix = test2.get_fake_data(user_preferences)
 
     iteration = 1
 
-    start, stop = get_start_and_stop(SF_sites)
+    start, stop = test2.get_start_and_stop(SF_sites)
 
     '''create an initial route'''
-    current_route = create_initial_route(start, stop, budget, tour_length, SF_sites, travel_matrix)
+    current_route = test2.create_initial_route(start, stop, budget, tour_length, SF_sites, travel_matrix)
 
     progress = []
     routes = []
@@ -47,10 +47,9 @@ def itin_generator(user_preferences, budget=2000, ambition = [9, 17], temperatur
     while (temperature > stopping_temperature) & (iteration < max_iterations):
         '''get a candidate'''
 
-        current_route = propagate_change(current_route, budget, tour_length, SF_sites, travel_matrix, temperature)
-
+        current_route = test2.propagate_change(current_route, budget, tour_length, SF_sites, travel_matrix, temperature)
         if (iteration % 50 == 0) :
-            print(iteration, temperature, progress[-1])
+            print(iteration, temperature, progress[-1], routes[-1])
 
             '''not bonanza!!!!! raise temperature by '''
             if len(progress) > 50 and (progress[-50] == progress[-1]):
@@ -67,16 +66,16 @@ def itin_generator(user_preferences, budget=2000, ambition = [9, 17], temperatur
 
         routes.append(current_route)
 
-
-    '''create the plot'''
-    plt.plot(progress)
-    plt.ylabel('Fitness Score')
-    plt.xlabel('Iteration')
-    plt.show()
-
     best_score_index = np.argmax(np.array(progress))
     best_score = progress[best_score_index]
     best_route = routes[best_score_index]
     print(best_score, best_route)
+
+
+    '''create the plot'''
+    # plt.plot(progress)
+    # plt.ylabel('Fitness Score')
+    # plt.xlabel('Iteration')
+    # plt.show()
 
     return progress, route, best_score_index
